ed4c7c3e48f20c5d3b9bf010cdb74cbd2c1402e9
Lucas Araújo Amorim <eligadowb@gmail.com>
2025-05-15 19:10:19 -0400

    Revert "Merge pull request #3 from eligado/feature/nicolas"
    This reverts commit f5bcb594df2b1f6b915277e37c5981509f076c41, reversing
    changes made to 1c1a2eb02bd4d2293db8e90d0ed341e23110daa7.

--- api/admin.py
+++ api/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from .models import Hospitais, Comentario
+from .models import Hospitais
 
 class HospitaisAdmin(admin.ModelAdmin):
     list_display = ('nome', 'endereco', 'hora_funcionamento', 'especialidades',)
-    search_fields = ('nome',)
+    search_fields= ('nome',)
 
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'hospital', 'estrelas', 'criado_em')
-    search_fields = ('usuario__username', 'hospital__nome')
-    list_filter = ('estrelas', 'criado_em')
-
-# Registrar os modelos no Django Admin (APENAS UMA VEZ)
+# Register your models here.
 admin.site.register(Hospitais, HospitaisAdmin)
-admin.site.register(Comentario, ComentarioAdmin)
--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib import admin
-from rest_framework.routers import DefaultRouter
-from .views import hospitais_list, ComentarioViewSet, comentarios_por_hospital
+from .views import hospitais_list
 
 admin.site.site_header = 'Administração do Conexão Hospitalar'
 admin.site.site_title = 'Administração do Conexão Hospitalar'
 admin.site.index_title = ''
 
-router = DefaultRouter()
-router.register(r'comentarios', ComentarioViewSet)
-
 urlpatterns = [
     path('hospitais/', hospitais_list, name='hospitais_list'),
-    path('hospitais/<int:hospital_id>/comentarios/', comentarios_por_hospital, name='comentarios_por_hospital'),
-    path('auth/', include('dj_rest_auth.urls')),  # login/logout
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),  # cadastro de usuário
-    path('', include(router.urls)),
 ]
